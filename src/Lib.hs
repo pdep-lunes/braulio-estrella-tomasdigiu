23a04b80b1f37901f3df29d7f8a256b48d88bffb
tomasdigiu <sebasdigiuseppeybarrios@utn.frba.edu.ar>
2025-04-28 11:31:44 -0300

    Creado tipo de dato Personaje y personaje Espina

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,3 +4,14 @@ import Text.Show.Functions ()
 
 doble :: Int -> Int
 doble x = x * 2
+
+data Personaje = UnPersonaje {
+    nombre :: String,
+    poderBasico :: String, 
+    superPoder :: String,
+    tieneSuperPoder :: Bool,
+    cantidadDeVida :: Int
+} deriving Show
+
+espina :: Personaje
+espina = UnPersonaje "Espina" "Bola de Espinas" "Granada de Espinas" True 4800
