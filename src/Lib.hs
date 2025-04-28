bd629774c26052ea480b3530e384bbeb224c8ead
tomasdigiu <sebasdigiuseppeybarrios@utn.frba.edu.ar>
2025-04-28 12:23:12 -0300

    Agregados poderes (a modificar)

--- src/Lib.hs
+++ src/Lib.hs
@@ -14,4 +14,24 @@ data Personaje = UnPersonaje {
 } deriving Show
 
 espina :: Personaje
-espina = UnPersonaje "Espina" "Bola de Espinas" "Granada de Espinas" True 4800
+espina = UnPersonaje "Espina" "Bola de Espinas" "Granada de Espinas Radio 5" True 4800
+
+pamela :: Personaje
+pamela = UnPersonaje "Pamela" "Lluvia de Tuercas Sanadoras" "Torreta Curativa" False 9600
+
+bolaEspinosa :: Int -> Int
+bolaEspinosa vida
+  | vida >= 1000 = vida - 1000
+  | otherwise = 0
+
+lluviaDeTuercas :: String -> Int -> Int
+lluviaDeTuercas tipoDeTuercas vida
+  | tipoDeTuercas == "Sanadoras" = vida + 800
+  | tipoDeTuercas == "Dañinas" = div vida 2
+  | otherwise = vida
+
+granadaDeEspinas :: Personaje -> Int -> Personaje
+granadaDeEspinas unPersonaje radio
+  | radio > 3 =  unPersonaje {nombre = nombre ++ " Espina estuvo aqui"}
+  | radio > 3 && cantidadDeVida unPersonaje < 800 = UnPersonaje 
+  | otherwise = unPersonaje
